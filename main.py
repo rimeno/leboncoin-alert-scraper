6e1e304f58f0f474866ca82a358ec29bcd939979
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2016-09-13 16:30:34 +0200

    change user agent on request

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import email_me, utils, settings
 # Iterate over all URLS from settings
 for url in settings.URLS :
 	# Load html page
-	page = requests.get(settings.URLS[url])
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	page = requests.get(settings.URLS[url], headers=headers)
 	tree = html.fromstring(page.content)
 	# Select all <a/> elements by class name
 	links_el = tree.xpath('//a[@class="list_item clearfix trackable"]')
@@ -20,9 +21,9 @@ for url in settings.URLS :
 		# Read last alert time
 		last_alert_time = utils.read_file('last_alert_'+ url +'.txt')
 		for i, el in enumerate(reversed(links_el), start=0):
-			# Get date element of classified ad
+			# Get date element of ad
 			date = el.xpath('.//p[@class="item_supp"]/text()')
-			# Get link element of classified ad
+			# Get link element of ad
 			link = tree.xpath('.//a[@class="list_item clearfix trackable"]/@href')
 			day_hour = ''
 			for d in date:
@@ -35,15 +36,14 @@ for url in settings.URLS :
 				day = splitted_day_hour[0]
 				hour = splitted_day_hour[1]
 				if len(splitted_day_hour) == 2:
-					# If the classified ad is from today
+					# If the ad is from today
 					if day == "Aujourd'hui":
 						# And is not the same and newer than the last
 						if hour != last_alert_time and hour > last_alert_time:
 							link = link[0][2:-8]
 							# Here I'm sending an email but you can do whatever you want, for exemple connect it to IFTTT maker channel, or send you a tweet
 							# Send the email
-							# First param is the object of the mail, second param is his content
-							email_me.send_email("Alerte "+ url +" !", "Nouvelle annonce detectée à : " + time.strftime('%H:%M:%S')+".\n" + link)
+							email_me.send_email("Alerte "+ url +" !", "Nouvelle annonce detectée à : " + time.strftime('%H:%M:%S')+".\n"+ link)
 							# Save the new alert hour
 							utils.write_to_file('last_alert_'+ url +'.txt', hour)
 							print("email send, new last_alert_time : ", hour)
